ade3fed93ac653d085ff842bea68fd9927e3960d
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-14 07:38:25 +0000

    Add routes to the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :transaract_groups
+  resources :groups
+  resources :transaracts
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index"
 end
