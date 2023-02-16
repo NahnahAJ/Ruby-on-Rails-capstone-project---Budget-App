b9983c9fe15e4314015ea35f7521dabb518c0f70
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-16 03:15:50 +0000

    Add index, create and new method's to the Groups controller for the Categories page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,31 @@
+# Home Page Categories Page 
+# When the user logs in, they are presented with the categories page.
+# For each category, the user can see their name, icon and the total amount of all the transactions that belongs to that category.
+# When the user clicks (or taps) on a category item, the application navigates to the transactions page for that category.
+# There is a button "add a new category" at the bottom that brings the user to the page to create a new category.
 class GroupsController < ApplicationController
-  def index; end
+  def index
+    @categories = Group.includes(:transaract_groups, :transaracts)
+    @transaracts = Transaract.all
+    @total_price = @transaracts.map { |all_prices| all_prices[:amount] }.sum
+  end
+
+  def new 
+    @new_category = Group.new
+  end
+
+  def create
+    @create_category = Group.create(create_category_params)
+    if @create_category.save
+      redirect_to groups_path
+    else
+      render :new 
+    end
+  end
+
+  private
+
+  def create_category_params
+    params.permit(:name, :icon, :author_id)
+  end
 end
