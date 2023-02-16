55500e40ee62c8afcf775b5f6ef08cecc4b81199
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-16 03:17:53 +0000

    Add routes for the pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :user
   resources :transaract_groups
-  resources :groups
+  resources :groups, only: [:index, :create, :new, :destroy] do
   resources :transaracts
+  end
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
+  root "groups#index"
 end
