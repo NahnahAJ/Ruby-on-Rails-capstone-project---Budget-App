76f5491826b50f921041e1f905165b2714e314ef
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-17 15:59:32 +0000

    Created transaract_groups

--- app/controllers/transaracts_controller.rb
+++ app/controllers/transaracts_controller.rb
@@ -12,11 +12,12 @@ class TransaractsController < ApplicationController
 
   def new
     @new_transaract = Transaract.new
+    @groups = Group.where(author: current_user)
   end
 
   def create
     @create_transaract = Transaract.create(name: params[:name], amount: params[:amount], author: current_user)
-    if @create_transaract.save
+    if @create_transaract
       @new_transaract_group = TransaractGroup.create(transaract_id: @create_transaract.id, group_id: params[:group_id])
       redirect_to groups_path
     else
@@ -24,3 +25,4 @@ class TransaractsController < ApplicationController
     end
   end
 end
+
